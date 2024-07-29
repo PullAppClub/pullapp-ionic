46fb46dce1bae2d22089b7885bce81c98fd397eb
elcrostatina <pleasedontcry@pm.me>
2024-07-29 20:40:52 +0200

    feat: desktop home navbar

--- src/app/shared/components/organisms/info-navbar/info-navbar.component.ts
+++ src/app/shared/components/organisms/info-navbar/info-navbar.component.ts
@@ -7,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoNavbarComponent implements OnInit {
   public showAppFlyout = false;
+  public showMuscleFlyout = false;
+  public showBusinessFlyout = false;
+  public showCompanyFlyout = false;
 
   constructor() {}
 
-  ngOnInit() {}
+  public ngOnInit() {}
+
+  public toggleAppFlyout(): void {
+    this.showAppFlyout = true;
+
+    this.showMuscleFlyout = false;
+    this.showBusinessFlyout = false;
+    this.showCompanyFlyout = false;
+  }
+
+  public toggleMuscleFlyout(): void {
+    this.showMuscleFlyout = true;
+
+    this.showAppFlyout = false;
+    this.showBusinessFlyout = false;
+    this.showCompanyFlyout = false;
+  }
+
+  public toggleBusinessFlyout(): void {
+    this.showBusinessFlyout = true;
+
+    this.showAppFlyout = false;
+    this.showMuscleFlyout = false;
+    this.showCompanyFlyout = false;
+  }
+
+  public toggleCompanyFlyout(): void {
+    this.showCompanyFlyout = true;
+
+    this.showAppFlyout = false;
+    this.showMuscleFlyout = false;
+    this.showBusinessFlyout = false;
+  }
 }
